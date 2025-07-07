b9f3372e15b7e53d040977da92d5fe2b4428faac
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-07 10:36:44 +0200

    fetchData function declared

---
+++ src/api/fetchData.ts
@@ -0,0 +1,11 @@
+
+
+export async function fetchData(
+  url: string,
+  headers: Record<string, string> = {},
+  type: 'json' | 'text' = 'json'
+): Promise<any> {
+  const response = await fetch(url, { headers });
+  if (!response.ok) throw new Error(`Fetch failed: ${url}`);
+  return type === 'json' ? response.json() : response.text();
+}
