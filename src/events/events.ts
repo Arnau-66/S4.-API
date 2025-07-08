3a3e49d42f1652d274d7b8a534fea708bafb5f7c
Arnau Perez <arnauperezesteve@gmail.com>
2025-07-08 10:27:18 +0200

    cleaning code commit

--- src/events/events.ts
+++ src/events/events.ts
@@ -7,11 +7,14 @@ import { JokeResponse } from "../types/types";
 
 export function attachJokeEvent(button: HTMLButtonElement): void {
   button.addEventListener("click", async () => {
+
     try {
-      const randomAPI = Math.random() < 0.5 ? APIs.dadJoke : APIs.chuckNorris;
-      const jokeData: JokeResponse = await fetchData(randomAPI.url, randomAPI.headers, randomAPI.type);
-      const joke = jokeData.joke || jokeData.value || "No joke found";
+      let randomAPI = Math.random() < 0.5 ? APIs.dadJoke : APIs.chuckNorris;
+      let jokeData: JokeResponse = await fetchData(randomAPI.url, randomAPI.headers, randomAPI.type);
+      let joke = jokeData.joke || jokeData.value || "No joke found";
+
       updateText("jokeDisplay", joke);
+
     } catch (err) {
       updateText("jokeDisplay", "Error loading joke");
       console.error(err);
@@ -21,9 +24,12 @@ export function attachJokeEvent(button: HTMLButtonElement): void {
 
 export function attachWeatherEvent(button: HTMLButtonElement): void {
   button.addEventListener("click", async () => {
+
     try {
-      const weather = await fetchData(APIs.weather.url, APIs.weather.headers, APIs.weather.type);
+      let weather = await fetchData(APIs.weather.url, APIs.weather.headers, APIs.weather.type);
+
       updateText("weatherDisplay", weather);
+      
     } catch (err) {
       updateText("weatherDisplay", "Error loading weather");
       console.error(err);
